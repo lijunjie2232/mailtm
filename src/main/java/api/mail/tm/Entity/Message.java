32f68132f2de2ae04e4e378634f8a9639a45a62e
lijunjie2232 <2533584225@qq.com>
2025-03-23 17:32:31 +0900

    mailtm

--- src/main/java/api/mail/tm/Entity/Message.java
+++ src/main/java/api/mail/tm/Entity/Message.java
@@ -1,4 +1,37 @@
 package api.mail.tm.Entity;
 
-public class Message {
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Message {
+    @SerializedName("@id")
+    private String id;
+
+    @SerializedName("@type")
+    private String type;
+
+    @SerializedName("@context")
+    private String context;
+
+    private String accountId;
+    private String msgid;
+    private EmailAddress from;
+    private List<EmailAddress> to;
+    private String subject;
+    private String intro;
+    private boolean seen;
+    private boolean isDeleted;
+    private boolean hasAttachments;
+    private int size;
+    private String downloadUrl;
+    private Date createdAt;
+    private Date updatedAt;
 }
